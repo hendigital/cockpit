f602105016d54f57b936eba71e2e396087894eef
uzma24 <inayauzma828@gmail.com>
2021-10-08 09:39:31 +0530

    solution commit

---
+++ ui/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+ReactDOM.render(<React.StrictMode><App /></React.StrictMode>,document.getElementById('root'));
+reportWebVitals();
+
+//ReactDOM.render(<h1>Hello World!!</h1>,document.getElementById('root'));
+
